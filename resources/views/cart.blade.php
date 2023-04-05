b11f995d8cb7866d5f023ae839d981afab900664
vdanviel <victordn.araujo@gmail.com>
2023-04-05 15:28:14 -0300

    cart complete

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -16,6 +16,17 @@
                     @endcomponent
                 @endif
 
+                @if (empty($products))
+                    
+                    <div class="d-flex p-4 align-items-center flex-column border rounded-3">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="60" height="60" fill="currentColor" class="bi bi-bag-x-fill" viewBox="0 0 16 16">
+                            <path fill-rule="evenodd" d="M10.5 3.5a2.5 2.5 0 0 0-5 0V4h5v-.5zm1 0V4H15v10a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V4h3.5v-.5a3.5 3.5 0 1 1 7 0zM6.854 8.146a.5.5 0 1 0-.708.708L7.293 10l-1.147 1.146a.5.5 0 0 0 .708.708L8 10.707l1.146 1.147a.5.5 0 0 0 .708-.708L8.707 10l1.147-1.146a.5.5 0 0 0-.708-.708L8 9.293 6.854 8.146z"/>
+                        </svg>
+                        <h4 class="mt-5">Não há produtos no carrinho.</h4>
+                    </div>
+
+                @else
+
                 <table class="table">
 
                     <thead class="table-primary">
@@ -66,6 +77,9 @@
                         
                     </tbody>
                 </table>
+
+                @endif
+
             </div>
 
             <div class="col mt-4 d-flex justify-content-center">
